3d72044cc90eaa4ee5f5daa5a7f6656e7dd25210
adrian.surducan@ic-consult.com <adrian.surducan@ic-consult.com>
2025-12-04 15:42:00 +0100

    Add Customization

---
+++ SamplePlugin/CustomizedAPI.cs
@@ -0,0 +1,22 @@
+﻿using QBM.CompositionApi.Crud;
+using QBM.CompositionApi.DataSources;
+using QBM.CompositionApi.Definition;
+using QBM.CompositionApi.Handling;
+using QER.CompositionApi.Portal;
+using System;
+using System.Globalization;
+
+namespace Api
+{
+    public class CustomizedAPI : IApiProviderFor<PortalApiProject>
+    {
+        public void Build(IApiBuilder builder)
+        {
+            builder.ModifyQueryMethod(​"/portal/itshop/requests",​ method => {​
+	            method.WithCalculatedProperties(new CalculatedProperty<string>(​"InternalNameUpper"​ context => context.Entity.GetValue("InternalName").String.ToUpperInvariant()​)​
+            }​
+            );
+        }
+
+    }
+}
