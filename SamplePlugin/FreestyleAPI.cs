1972aa0a057c476a3819fdf4f192fc9373be7df8
adrian.surducan@ic-consult.com <adrian.surducan@ic-consult.com>
2025-12-04 17:31:09 +0100

    Add Config params

--- SamplePlugin/FreestyleAPI.cs
+++ SamplePlugin/FreestyleAPI.cs
@@ -1,35 +1,94 @@
-﻿using QBM.CompositionApi.Crud;
+﻿using QBM.CompositionApi.Config;
+using QBM.CompositionApi.Crud;
 using QBM.CompositionApi.DataSources;
 using QBM.CompositionApi.DataSources.SqlWizard;
 using QBM.CompositionApi.Definition;
 using QBM.CompositionApi.Handling;
 using QER.CompositionApi.Portal;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using VI.DB;
 using VI.DB.Entities;
 using VI.DB.Sync;
 
 namespace Api
 {
+  
     public class FreestyleAPI : IApiProviderFor<PortalApiProject>
     {
+
+        [DisplayName("Custom configuration")]
+
+        public class CustomConfiguration
+        {
+            [DisplayName("Identities returned number")]
+            [Description("Identities returned number changed")]
+            public int ReturnSize { get; set; } = 5;
+
+
+            [DisplayName("Identities name where clause")]
+            [Description("Identities name where clause")]
+            public string customWhereClause { get; set; } = "FirstName like N'B%'";
+
+
+            [DisplayName("Attributes to return")]
+            [Description("Attributes to return")]
+
+            [ConfigSettingType(ConfigSettingType.LimitedValues)]
+            [ConfigValueProvider(typeof(AttributesTypeProvider))]
+            public string[] attributes { get; set; } = ["FirstName", "LastName"];
+        }
+            internal class AttributesTypeProvider : IConfigSettingValueProvider
+             {
+                public async Task<IEnumerable<ConfigSettingValidValue>> GetValidValuesAsync(ISession session, IConfigSetting setting, CancellationToken ct)
+                {
+                return session.MetaData()
+                .GetTable("Person")
+                .Columns
+                .Select(c => new ConfigSettingValidValue
+                {
+                    Display = c.Columnname,
+                    Value = c.Columnname
+                })
+                .ToArray();
+            }
+            }
+
+        private CustomConfiguration customConfig;
+       
+          
+        
         public void Build(IApiBuilder builder)
         {
+            var svc = builder.Resolver.Resolve<IConfigService>();
+            customConfig = new CustomConfiguration();
+            svc.RegisterConfigurableObject(customConfig);
+         
+
+
+
             builder.AddMethod(Method.Define("freestyleEx1")
                 .HandleGet(async request =>
                 {
-                    var collection = await request.Session.Source().GetCollectionAsync(Query.From("Person").Take(5).OrderBy("FirstName").Select("FirstName", "LastName")).ConfigureAwait(false);
+                    var collection = await request.Session.Source().GetCollectionAsync(Query.From("Person").Take(this.customConfig.ReturnSize).Where(this.customConfig.customWhereClause).OrderBy("FirstName").Select(this.customConfig.attributes)).ConfigureAwait(false);
                    
                     Identity[] names = [];
 
                     foreach (var Identity in collection)
                     {
                         Identity returnedPerson = new Identity();
-                        returnedPerson.FirstName = Identity.GetValue("FirstName").ToString();
-                        returnedPerson.LastName = Identity.GetValue("LastName").ToString();
+                        
+                        foreach (var attr in this.customConfig.attributes)
+                        {
+                            var value = Identity.GetValue(attr);
+                            returnedPerson.DynamicFields[attr] = value?.ToString();
+                        }
 
                         names = names.Append(returnedPerson).ToArray();
                      
@@ -155,5 +214,6 @@ namespace Api
         public string NewName { get; set; }
         public string ESet { get; set; }
         public string EntryDateWeekday { get; set; }
+        public Dictionary<string, object> DynamicFields { get; set; } = new();
     }
 }
